ed552130bde4f84648157e861d1a94fb22fc0920
cocksure <sanjarwer93@gmail.com>
2023-07-07 11:42:58 +0500

    change the model Book: cover_picture

--- books/models.py
+++ books/models.py
@@ -9,7 +9,7 @@ class Book(models.Model):
     title = models.CharField(max_length=200)
     description = models.TextField()
     isbn = models.CharField(max_length=17)
-    cover_picture = models.ImageField(default="default_cover.jpg")
+    cover_picture = models.ImageField(upload_to='media-files/', default="default_cover.jpg")
 
     def __str__(self):
         return self.title
