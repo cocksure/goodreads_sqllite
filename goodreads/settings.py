78a94eb78101e2c73e014cf9a367fbb8751c9d51
cocksure <sanjarwer93@gmail.com>
2023-07-21 15:41:04 +0500

    media

--- goodreads/settings.py
+++ goodreads/settings.py
@@ -124,17 +124,14 @@ USE_TZ = True
 
 AUTH_USER_MODEL = "users.CustomUser"
 
-MEDIA_URL = "/media/"
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media-files')
-
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/4.2/howto/static-files/
-
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     BASE_DIR / 'static'
 ]
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+
+MEDIA_URL = "/media/"
+MEDIA_ROOT = BASE_DIR / 'media'
 
 
 # Default primary key field type
